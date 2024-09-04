b26a823cb3c9e59f320b032817bdccd315459e7f
Phong Do <dominhphong306@gmail.com>
2024-09-04 07:58:48 +0700

    chore: add old files

---
+++ tests/lectures/javascript/01-hello.js
@@ -0,0 +1,13 @@
+switch (condition) {
+    case "<case_value_1>":
+        // code block 1
+        break;
+    case "<case_value_2>":
+        // code block 2
+        break;
+    case "<case_value_3>":
+        // code block 3
+        break;
+    default:
+        // code block 4
+}
--- tests/lectures/playwright-test/02-navigate.spec.ts
+++ tests/lectures/playwright-test/02-navigate.spec.ts
@@ -1,65 +1,20 @@
 import { test } from '@playwright/test';
 
-test('Basic action', async ({ page }) => {
-    await page.goto("https://material.playwrightvn.com/018-mouse.html");
+const registerPageUrl = "https://material.playwrightvn.com/01-xpath-register-page.html";
 
-    // Single click
-    await page.locator("//div[@id='clickArea']").click();
+test('date picker, slider, color picker', async ({ page }) => {
+    await page.goto(registerPageUrl);
 
-    // Double click
-    await page.locator("//div[@id='clickArea']").dblclick();
-
-    // Right click
-    // await page.locator("//div[@id='clickArea']").click({
-    //     button: 'right'
-    // });
-
-    // Phim khac
-    await page.locator("//div[@id='clickArea']").click({
-        modifiers: ["Shift", "Meta"]
-    });
-})
-
-test('basic input', async ({ page }) => {
-    await page.goto("https://material.playwrightvn.com/01-xpath-register-page.html");
-
-    await page.locator("//input[@id='username']").pressSequentially("udemy@gmail.com", {
-        delay: 100,
-    });
-
-    // Check radio female
-    await page.locator("//input[@id='female']").check();
-
-    // Check checkbox
-    await page.locator("//input[@id='reading']").check();
-    // await page.locator("//input[@id='traveling']").check();
-
-    // Uncheck
-    //await page.locator("//input[@id='traveling']").uncheck();
-
-    // Get check status
-    const isCheck = await page.locator("//input[@id='traveling']").isChecked();
-
-    console.log(isCheck);
+   await page.locator("//input[@id='dob']").fill("2024-01-30");
+   await page.locator("//input[@id='rating']").fill("3");
+   await page.locator("//input[@id='favcolor']").fill("#e66465");
 });
 
-test('select', async ({ page }) => {
-    await page.goto("https://material.playwrightvn.com/01-xpath-register-page.html");
 
-    await page.locator("//select[@id='country']").selectOption("Australia");
+test('file, hover', async ({ page }) => {
+    await page.goto(registerPageUrl);
 
-    await page.locator("//select[@id='interests']").selectOption(["Music", "Art"]);
-})
-
-test('date', async ({ page }) => {
-    await page.goto("https://material.playwrightvn.com/01-xpath-register-page.html");
-
-    await page.locator("//input[@id='username']").fill("udemy");
-
-    await page.locator("//input[@id='email']").fill("udemy@gmail.com");
-
-    await page.locator("//input[@id='dob']").fill("2024-01-30");
-
-
-    await page.locator("//button[@type='submit']").click();
-})
+   await page.locator("//input[@id='profile']").setInputFiles("tests/lectures/playwright-test/a1.txt");
+   await page.locator("//div[@class='tooltip']").hover();
+   
+});
